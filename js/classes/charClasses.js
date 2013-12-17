0fe287dd0d6fc2d4df0700b1f0ce56bc6b741f4f
Krister Bäckman <ixevix@gmail.com>
2013-12-17 23:00:58 +0200

    Optimize layout for mobile browsers

--- js/classes/charClasses.js
+++ js/classes/charClasses.js
@@ -4,19 +4,7 @@ var charClasses = {
 
 	renderSelects:function(){
 		for (var i=1; i<=game.partySize; i++) {
-			var ui_class;
-			switch(i){
-				case 1:
-					ui_class = "ui-block-a";
-					break;
-				case 2:
-					ui_class = "ui-block-b";
-					break;
-				case 3:
-					ui_class = "ui-block-c";
-					break;
-			}
-			var character_select = '<div id="character-'+i+'" class="character-container '+ui_class+'"><div class="ui-bar ui-bar-e">';
+			var character_select = '<div id="character-'+i+'" class="character-container"><div class="ui-bar ui-bar-e">';
 			character_select += '<h2 data-translation="lang1'+(i+5)+'"></h2>';
 			character_select += '<label for="character-'+i+'-name" data-translation="lang15"></label>';
 			character_select += '<input type="text" name="character-'+i+'-name"></input>';
